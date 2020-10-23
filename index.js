68f95985ca77df87ccf779abde0a67c498c365c3
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-10-22 19:47:23 -0700

    commit tree f516a2e023c4c9ec0d0c83a2d7a089d6a7dee8bd

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+console.log(dodger.style.left);
+console.log(dodger.style.bottom);
+
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "Arrowleft"){
+        let leftNumbers = dodger.style.left.replace("px", "");
+        let left = parseInt(leftNumbers, 10);
+        
+        if (left > 0){
+            dodger.style.left = `${left - 1}px`;
+        }
+    }
+});
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    
+    if (left > 0){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+moveDodgerLeft();
